a8fbeb51a0f804fd9806231ae2a36f8c90411967
Anthony <amano@amano.loebent.lan>
2017-08-19 13:50:48 -0400

    Cleaning up transitions and slide animations

--- js/slide.js
+++ js/slide.js
@@ -4,10 +4,24 @@ var arrowLeft = document.getElementById('arrow-left');
 var slideProgress = document.getElementById('slide__progress');
 
 var slideNames = ['SingleCare', 'MobiSave', 'LateShift', 'Thnks', 'BevRage']
+var slideNumber = 0;
 
-var currentSlide = 0;
-var progressFactor = 100 / slideNames.length;
-var progressNumber = -100 + progressFactor;
+console.log(slideNumber);
+
+var slideProgressBar = document.createElement('div');
+slideProgressBar.setAttribute('id', 'slideProgressBar');
+slideProgressBar.style.backgroundColor = 'black';
+slideProgressBar.style.bottom = '0';
+slideProgressBar.style.height = '16px';
+slideProgressBar.style.left = '0';
+slideProgressBar.style.opacity = '.16';
+slideProgressBar.style.position = 'absolute';
+slideProgressBar.style.transition = 'opacity .3s, width .3s';
+document.getElementById('slides__navigation').appendChild(slideProgressBar);
+
+var slideDots = document.createElement('ul');
+slideDots.setAttribute('id', 'slide__dots');
+document.getElementById('slides__navigation').appendChild(slideDots);
 
 for (var slideDotNumber = 0; slideDotNumber < slideNames.length; slideDotNumber++) {
   var slide = document.createElement('section');
@@ -40,7 +54,7 @@ for (var slideDotNumber = 0; slideDotNumber < slideNames.length; slideDotNumber+
   slideBtn.style.height = '16px';
   slideBtn.style.position = 'relative';
   slideBtn.style.transform = 'background-color .3s';
-  slideBtn.style.width = 'calc(' + progressFactor + '% - 2px)';
+  slideBtn.style.width = 'calc(' + (100 / slideNames.length) + '% - 2px)';
   document.getElementById('slide__dots').appendChild(slideBtn);
 
   var slideBtnLabel = document.createElement('p');
@@ -61,31 +75,25 @@ for (var slideDotNumber = 0; slideDotNumber < slideNames.length; slideDotNumber+
 }
 
 function slideShift() {
-  slideProgress.style.transform = 'translate3d(' + progressNumber + '%,0,0)';
-  slideContainer.style.transform = 'translate3d(' + currentSlide + '%,0,0)';
+  slideProgressBar.style.width = 100 * ((slideNumber + 1) / slideNames.length) + '%';
+  slideContainer.style.transform = 'translate3d(' + (-100 * (slideNumber)) + '%,0,0)';
 }
 slideShift();
 
 arrowRight.addEventListener('click', function() {
-  if (currentSlide <= (-(slideNames.length - 1) * 100)) {
-    currentSlide -= -(slideNames.length - 1) * 100;
-    progressNumber = -100 + progressFactor;
+  if (slideNumber >= slideNames.length - 1) {
+    slideNumber = 0;
   } else {
-    currentSlide -= 100;
-    progressNumber = progressNumber + progressFactor
+    slideNumber++;
   }
   slideShift();
-
 })
 
 arrowLeft.addEventListener('click',function() {
-  if (currentSlide >= 0) {
-    currentSlide -= (slideNames.length - 1) * 100;
-    progressNumber = 0;
+  if (slideNumber <= 0) {
+    slideNumber = slideNames.length - 1;
   } else {
-    currentSlide += 100;
-    progressNumber = progressNumber - progressFactor;
+    slideNumber--;
   }
   slideShift();
-
 })
